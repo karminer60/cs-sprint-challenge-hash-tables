7c8b80256be48229d9e86d8406145ab384519ad7
karina-rodriguez <karminer60@gmail.com>
2020-09-04 15:06:08 -0700

    working on ex1.py

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -4,4 +4,13 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     # Your code here
 
+    Dict = {}
+    result = []
+    for index in range(len(weights)):
+        Dict[weights[index]] = index  
+    for number in a:
+        if number > 0 and - number in Dict:
+            result.append(number)       
+    return result
+
     return None
